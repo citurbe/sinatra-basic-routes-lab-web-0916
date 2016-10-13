cb31712294c3a4dcda45b16734eab925d9af6a2a
citurbe <craigiturbe@Craigs-MacBook-Air.local>
2016-10-13 11:59:41 -0400

    Add routes for name, hometown, and favorite song

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Craig"
+  end
+
+  get '/hometown' do
+    "My hometown is Ardsley"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Holland, 1945"
+  end
 end
